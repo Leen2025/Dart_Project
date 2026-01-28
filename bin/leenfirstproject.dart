961187cee2a09476a4294ec99f2343b059ffaee5
Leen2025 <leeeen4@hotmail.com>
2026-01-28 08:39:08 +0300

    update

--- bin/leenfirstproject.dart
+++ bin/leenfirstproject.dart
@@ -2,4 +2,5 @@ import 'package:leenfirstproject/leenfirstproject.dart' as leenfirstproject;
 
 void main(List<String> arguments) {
   print('Hello world: ${leenfirstproject.calculate()}!');
+  //hello 
 }
