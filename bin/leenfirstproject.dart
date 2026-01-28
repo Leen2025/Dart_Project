ba9fcef0731726a20fcd9a5e26affeb12fc0ef7c
Leen2025 <leeeen4@hotmail.com>
2026-01-28 08:29:54 +0300

    update

---
+++ bin/leenfirstproject.dart
@@ -0,0 +1,5 @@
+import 'package:leenfirstproject/leenfirstproject.dart' as leenfirstproject;
+
+void main(List<String> arguments) {
+  print('Hello world: ${leenfirstproject.calculate()}!');
+}
---
+++ test/leenfirstproject_test.dart
@@ -0,0 +1,8 @@
+import 'package:leenfirstproject/leenfirstproject.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
